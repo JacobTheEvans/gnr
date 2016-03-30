8af41bf8ff09e188b56cd02b815ebfe7e89300c3
Jacob <jakegod14@gmail.com>
2016-03-30 09:13:37 -0600

    Init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#GNR
+<p>Galaxy News Radio</p>
+<p>A site that uses Youtube to steam music from the popular Bethesda game fallout 3</p>
---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+var app = angular.module("app", ["youtube-embed"]);
+
+app.controller("mainController", ["$scope", function($scope) {
+  $scope.currentSong = "WGmHaMRAXuI";
+}]);
